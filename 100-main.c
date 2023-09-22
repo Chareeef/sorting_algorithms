00919670ce69f90fd5276e8ee3a2bacd22baf5e9
Chareeef <youssef.charif.h@gmail.com>
2023-09-22 17:56:08 +0100

    Correct shell sort

--- 100-main.c
+++ 100-main.c
@@ -9,13 +9,33 @@
  */
 int main(void)
 {
-    int array[] = {19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
-    size_t n = sizeof(array) / sizeof(array[0]);
+	int array[] = {19, 8, 99, 51, 1, 83, 96, 43,19, 48, 99, 71, 13, 52, 96, 73, 86, 7};
+	size_t n = sizeof(array) / sizeof(array[0]);
+	int array2[] = {1, 1, 2, 3, 4, 5, 6, 6};
+	size_t n2 = sizeof(array2) / sizeof(array2[0]);
+	int array3[] = {9, 8, 7, 6, 6, 5};
+	size_t n3 = sizeof(array3) / sizeof(array3[0]);
 
-    print_array(array, n);
-    printf("\n");
-    shell_sort(array, n);
-    printf("\n");
-    print_array(array, n);
-    return (0);
+	print_array(array, n);
+	printf("\n");
+	shell_sort(array, n);
+	printf("\n");
+	print_array(array, n);
+
+	printf("\n\t[------------[\n");
+
+	print_array(array2, n2);
+	printf("\n");
+	shell_sort(array2, n2);
+	printf("\n");
+	print_array(array2, n2);
+
+	printf("\n\t[------------[\n");
+
+	print_array(array3, n3);
+	printf("\n");
+	shell_sort(array3, n3);
+	printf("\n");
+	print_array(array3, n3);
+	return (0);
 }
--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -6,32 +6,29 @@
  * @array: Array to be sorted
  * @size: The size of that array
  */
-
 void shell_sort(int *array, size_t size)
 {
-	size_t gap = 1, i, j;
-	int tmp = 0;
+	size_t gap, i, j;
+	int temp;
 
+	gap = 1;
 	while (gap < size / 3)
-	{
 		gap = gap * 3 + 1;
-	}
 
 	while (gap > 0)
 	{
 		for (i = gap; i < size; i++)
 		{
-			tmp = array[i];
-			j = i;
+			temp = array[i];
 
-			while (j >= gap && array[j - gap] > tmp)
+			for (j = i; j >= gap && array[j - gap] > temp; j -= gap)
 			{
 				array[j] = array[j - gap];
-				j -= gap;
 			}
 
-			array[j] = tmp;
+			array[j] = temp;
 		}
+
 		gap /= 3;
 		print_array(array, size);
 	}
